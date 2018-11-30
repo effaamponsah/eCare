edaf9b21e2c0d00cc6b97969d866f38f2ad897c1
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2018-11-30 12:51:12 +0000

    Project Startup

--- forms/p_d_submit.php
+++ forms/p_d_submit.php
@@ -1,31 +1,66 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hms";
+<?php 
+session_start();
+$firstname=filter_input(INPUT_POST, 'r-form-first-name');
+$lastname=filter_input(INPUT_POST, 'r-form-last-name');
+$username=filter_input(INPUT_POST, 'r-form-Username');
+$password=filter_input(INPUT_POST, 'r-form-Password');
+if (!empty($firstname)) {
+	if (!empty($lastname)) {
+		if (!empty($username)) {
+			if (!empty($password)) {
+				$host="localhost";
+				$dbusername="root";
+				$dbpassword="";
+				$dbname="hms";
+				$conn=new mysqli ($host, $dbusername, $dbpassword, $dbname);
+				if (mysqli_connect_error()) {
+					die('Connect Error ('. mysqli_connect_errno() .') ' . mysqli_connect_error());
+				}
+				else {
+					$sql="INSERT INTO staff (Fname, Lname, UName, Password)
+ 					values ('$firstname', '$lastname', '$username', '$password')";
+ 				if ($conn->query($sql)) {
+						echo "You have signed up Succesfully";
+						$_SESSION["username"] = $username;
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+						// implement the same as the login for the 
+						// get the first cahracter and make a route based on that.
+						$first_char = $username[0];
 
-$name =filter_input(INPUT_POST, 'p_name');
-/* $weight =filter_input(INPUT_POST, 'weight');
-$temp =filter_input(INPUT_POST, 'temp');
-$bp = filter_input(INPUT_POST, 'bp'); */
+						if ($first_char == 'N') {
+							header('Location:../../B1');  
+							die(); 
+						}
+						else {
+							header('Location: ../../doc.html');
+							die();
+						}
 
-/* $sql = "INSERT INTO patients_data (Name, Weight, Temperature, BP)
-VALUES ('$name', '$weight', '$temp', $bp)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+				}
+				else {
+						echo "Sign up failed";
+						
+				}
+					$conn->close();
+				}
+			}
+			else {
+				echo "This field cannot be empty";
+			}
+		}
+		else {
+			echo "This field cannot be empty";
+			die();
+		}
+	}
+	else {
+		echo "This field cannot be empty";
+		die();
+	}
 }
 
-$conn->close(); */
-
-echo $name
+else {
+	echo "This field cannot be empty";
+	die();
+}
 ?>
